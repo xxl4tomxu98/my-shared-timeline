47cb86b07b0b99a33ee6307324a39495072def9d
Tom Xu <txu@guidehouse.com>
2022-04-13 21:48:34 -0400

    added linkedlist interview

---
+++ data_structure_algorithm/data-structures-linked-list-starter/lib/linked_list.js
@@ -0,0 +1,191 @@
+// ============================================================================
+// Implementation Exercise: Singly Linked List
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Implement a Singly Linked List and all of its methods below!
+//
+// ------------
+// Constraints:
+// ------------
+//
+// Make sure the time and space complexity of each is equivalent to those
+// in the table provided in the Time and Space Complexity Analysis section
+// of your Linked List reading!
+//
+// -----------
+// Let's Code!
+// -----------
+
+// TODO: Implement a Linked List Node class here
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+
+}
+
+// TODO: Implement a Singly Linked List class here
+class LinkedList {
+    constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    // TODO: Implement the addToTail method here
+    addToTail(val) {
+        let node = new Node(val);
+        if (this.length === 0) {
+            this.head = node;
+        }
+        if (this.length > 0) {
+            this.tail.next = node;
+        }
+        this.tail = node;
+        this.length++;
+        return this;
+    }
+
+    // TODO: Implement the removeTail method here
+    removeTail() {
+        if (this.length === 0) {
+            return undefined;
+            }
+            let popped = this.tail;
+            if (this.length === 1) {
+                this.head = null;
+                this.tail = null;
+                this.length = 0;
+                return popped;
+            }
+            let node = this.head;
+            while(node.next !== this.tail){
+                node = node.next;
+            }
+            node.next = null;
+            this.tail = node;
+            this.length--;
+            return popped;
+    }
+
+    // TODO: Implement the addToHead method here
+    addToHead(val) {
+        let node = new Node(val);
+        if (this.length === 0) {
+          this.head = node;
+          this.tail = node;
+        } else {
+          node.next = this.head;
+          this.head = node;
+        }
+        this.length++;
+        return this;
+    }
+
+    // TODO: Implement the removeHead method here
+    removeHead() {
+        if (this.length === 0) {
+            return undefined;
+        }
+        let oldHead = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+            this.length = 0;
+            return oldHead;
+        }
+        this.head = this.head.next;
+        this.length--;
+        return oldHead;
+    }
+
+    // TODO: Implement the contains method here
+    contains(target) {
+        let node = this.head;
+        while (node !== null) {
+            if (node.value === target) {
+                return true;
+            }
+            node = node.next;
+        }
+        return false;
+    }
+
+    // TODO: Implement the get method here
+    get(index) {
+        if (index > this.length || index <0) {
+            return null;
+        }
+        let count = 0;
+        let node = this.head;
+        while (count < index) {
+            node = node.next;
+            count++;
+        }
+        return node;
+    }
+
+    // TODO: Implement the set method here
+    set(index, val) {
+        let count = 0;
+        let node = this.head;
+        while (count < index) {
+            node = node.next;
+            count++;
+            if (node === null) {
+                return false;
+            }
+        }
+        node.value = val;
+        return true;
+    }
+
+    // TODO: Implement the insert method here
+    insert(index, val) {
+        if (index > this.length || index < 0) {
+            return false;
+        }
+        let count = 0;
+        let node = this.head;
+        while (count < index - 1) {
+            node = node.next;
+            count++;
+        }
+        let oldNext = node.next;
+        let newNode = new Node(val);
+        newNode.next = oldNext;
+        node.next = newNode;
+        this.length++;
+        return true;
+    }
+
+    // TODO: Implement the remove method here
+    remove(index) {
+        if (index > this.length || index < 0) {
+            return undefined;
+        }
+        let count = 0;
+        let node = this.head;
+        while (count < index - 1) {
+            node = node.next;
+            count++;
+        }
+        let removed = node.next;
+        node.next = node.next.next;
+        this.length--;
+        return removed;
+    }
+
+    // TODO: Implement the size method here
+    size() {
+        return this.length;
+    }
+}
+
+exports.Node = Node;
+exports.LinkedList = LinkedList;
---
+++ data_structure_algorithm/data-structures-linked-list-starter/linked_list_interview_problems/lib/2_linked_list_intersection.js
@@ -0,0 +1,116 @@
+// ============================================================================
+// Interview Problem: Linked List Intersection
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Write a function linkedListIntersection that returns the node at which the 
+// intersection of two linked lists begins, or null if there is no such 
+// intersection.
+//
+// ---------- 
+// Example 1:
+// ----------
+// 
+// Given the following two linked lists, list1 and list2, 
+// linkedListIntersection(list1,list2) should return D 
+// as the node of intersection.
+// 
+//    A → B → C
+//             ↘
+//               D → E → F
+//             ↗
+//        X → Y
+//
+// ---------- 
+// Example 2:
+// ----------
+//
+// Given the following two linked lists, list1 and list2, 
+// linkedListIntersection(list1, list2) should return null 
+// as there is no point of intersection.
+// 
+//    A → B → C → D
+//
+//    X → Y → Z
+// 
+// -----------
+// Let's code!
+// -----------
+function linkedListIntersection(list1, list2) {
+  // TODO: Implement the hasCycle function!
+  if(list1.length===0 || list2.length===0){
+    return null;
+  }
+  let node1 = list1.head;
+  let node2 = list2.head;
+  if(node1===node2) return node1; 
+  while(node1.next && node2.next){
+    node1 = node1.next;
+    node2 = node2.next;
+    if(node1===node2) return node1;
+  }
+  return null;
+}
+
+// ----------------------------------------
+// Given: Singly Linked List - Do Not Edit!
+// ----------------------------------------
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  addToTail(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+    }
+
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+}
+
+// --------------------------------------
+// Helper For Testing Only - Do Not Edit!
+// --------------------------------------
+var stringify = function(list) {
+  var result = [];
+  while(list !== null) {
+    result.push(list.value);
+    list = list.next;
+  }
+  return result.join("");
+}
+
+exports.Node = Node;
+exports.LinkedList = LinkedList;
+exports.linkedListIntersection = linkedListIntersection;
+exports.stringify = stringify;
